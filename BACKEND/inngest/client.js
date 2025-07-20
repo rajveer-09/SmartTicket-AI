e8070fb2adf487cb9b891ab78632cb07a0775720
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-07-20 08:50:07 +0530

    Update client.js

--- BACKEND/inngest/client.js
+++ BACKEND/inngest/client.js
@@ -1,6 +1,5 @@
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
-    id : "ticketing-system",
-    signingKey: process.env.INGGEST_SIGNING_KEY
+    id : "ticketing-system"
 });
